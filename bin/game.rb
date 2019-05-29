262a2bc7af7c2bc0f1146b7395be3d653f26a994
Kelvin Liang <kelvin9877@gmail.com>
2019-05-29 23:41:51 +0800

    add play again feature (first draft)

--- bin/game.rb
+++ bin/game.rb
@@ -24,16 +24,29 @@ until game_on == false
   inputOne = game.play_game("X", name_1, positions, inputOne)
 
   if game.wins?(inputOne)
-    puts "You win #{name_1}!"
     game.display(positions)
-    break;
+    puts "You win #{name_1}!"
+    game_on = false
   elsif game.if_full?
     puts "Game board is full!"
-    break;
+    game_on = false
+  end
+
+  if game_on == false
+    puts "#{name_1}, would you like to play again?(y/n)?" 
+    play_again = gets.chomp
+    if play_again == "y"
+      positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      inputOne = []
+      inputTwo = []
+      game_on = true
+      next
+    end
+    break
   end
   
   inputTwo = game.play_game("O", name_2, positions,inputTwo)
-
+  
   if game.wins?(inputTwo)
     puts "You win #{name_2} !"
     game.display(positions)
@@ -42,6 +55,20 @@ until game_on == false
     puts "Game board is full!"
     game_on = false
   end  
+  
+  if game_on == false
+    puts "#{name_2}, would you like to play again?(y/n)?" 
+    play_again = gets.chomp
+    if play_again == "y"
+      positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      inputOne = []
+      inputTwo = []
+      game_on = true
+      next
+    end
+    break
+  end
+
 end
 
 
