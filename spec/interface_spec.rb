8be3c5b31227434bf3463e7650a72d6de700cead
Denis Cuenca Brex <Deniscbrex.dev@gmail.com>
2019-06-10 11:46:04 -0300

    added some test in interface

---
+++ spec/interface_spec.rb
@@ -0,0 +1,36 @@
+require "./lib/interface"
+
+RSpec.describe Interface do
+    # describe "#show" do
+    #     it "Display the board positions" do
+    #         expect(show([1,2,3,4,5,6,7,8,9])).to output
+    #     end
+    # end
+
+    include Interface
+    
+    describe "#get_input" do
+        it "Display text when game is finished" do
+            expect(get_input('finish')).to output("Thanks for your time! \n")
+        end
+
+        it "Display text when you have to enter position" do
+            expect(get_input('position')).to eql(gets.chomp)
+        end
+
+        it "Display text when you quit game" do
+            expect(get_input('quit')).to output("Sorry to see you go, see you next time! \n")
+        end
+    end
+    
+    describe "#valid_number?" do
+        it "If input a valid number" do
+            expect(valid_number?(22)).to eql(true)
+        end
+
+        it "If input is valid" do
+            expect(valid_number?(1)).to be true
+        end
+    end
+
+end
